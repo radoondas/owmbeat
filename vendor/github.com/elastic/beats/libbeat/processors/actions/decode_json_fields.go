58676e5e8993b24f48ce2512e1e1d2c78970e1d5
radoondas <ondas.radovan@gmail.com>
2020-02-28 16:53:33 +0100

    Upgrade to 7.6.0

--- vendor/github.com/elastic/beats/libbeat/processors/actions/decode_json_fields.go
+++ vendor/github.com/elastic/beats/libbeat/processors/actions/decode_json_fields.go
@@ -68,7 +68,7 @@ func init() {
 	processors.RegisterPlugin("decode_json_fields",
 		checks.ConfigChecked(NewDecodeJSONFields,
 			checks.RequireFields("fields"),
-			checks.AllowedFields("fields", "max_depth", "overwrite_keys", "add_error_key", "process_array", "target", "when", "document_id")))
+			checks.AllowedFields("fields", "max_depth", "overwrite_keys", "add_error_key", "process_array", "target", "when")))
 
 	jsprocessor.RegisterPlugin("DecodeJSONFields", NewDecodeJSONFields)
 }
